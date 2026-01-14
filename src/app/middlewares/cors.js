6e34b27c51d6db0a018e97e366f0672d1273ea30
Atanazio01 <marcosd.atanazio@gmail.com>
2026-01-14 11:43:42 -0300

    feature: allowing multiple sources in CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,8 +1,16 @@
 module.exports = (request, response, next) => {
   // Wildcard -> Curinga -> *
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const allowedOrigins = ["http://localhost:3000", "http://localhost:3002"];
+
+  const { origin } = request.headers;
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader("Access-Control-Allow-Origin", origin);
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    response.setHeader("Access-Control-Max-Age", "10");
+  }
   next();
 };
